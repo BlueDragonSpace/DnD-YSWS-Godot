0136e5dca532abe55e9e28888d41fcef768a563c
Eli Cobine <185584826+BlueDragonSpace@users.noreply.github.com>
2026-04-06 15:31:05 -0700

    using Javascript functions through GDScript

--- Godot/main.gd
+++ Godot/main.gd
@@ -2,3 +2,10 @@ extends Node2D
 
 func _on_linky_pressed() -> void:
 	OS.shell_open("https://hackclub.com/")
+
+
+func _ready():
+	# Retrieve the `window.console` object.
+	var console = JavaScriptBridge.get_interface("console")
+	# Call the `window.console.log()` method.
+	console.log("logged a message using Javascript, through GDScript!")
