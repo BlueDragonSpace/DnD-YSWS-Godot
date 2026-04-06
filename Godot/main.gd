8ec152bcef87478896f220fb25a7be0e70de64b9
Eli Cobine <185584826+BlueDragonSpace@users.noreply.github.com>
2026-04-06 16:07:46 -0700

    more export testing

--- Godot/main.gd
+++ Godot/main.gd
@@ -5,7 +5,13 @@ func _on_linky_pressed() -> void:
 
 
 func _ready():
-	# Retrieve the `window.console` object.
-	var console = JavaScriptBridge.get_interface("console")
-	# Call the `window.console.log()` method.
-	console.log("logged a message using Javascript, through GDScript!")
+	## Retrieve the `window.console` object.
+	#var console = JavaScriptBridge.get_interface("console")
+	## Call the `window.console.log()` method.
+	#console.log("logged a message using Javascript, through GDScript!")
+	
+	JavaScriptBridge.eval("alert('Calling Javascript per GDScript!');")
+
+
+func _on_javascript_log_pressed() -> void:
+	JavaScriptBridge.eval("window.console.log('calling thru the windowwwwww');")
