033bfa513f4c18ac122033619f01c188f25e88d5
Eli Cobine <185584826+BlueDragonSpace@users.noreply.github.com>
2026-04-06 18:13:36 -0700

    adding 3d falling dice thing

---
+++ Godot/3dStuff/dice_spawner.gd
@@ -0,0 +1,12 @@
+extends Node3D
+
+@export var dice : PackedScene = null
+@export var rand_pos = Vector3(5, 0, 5) # random position
+# spawns at same position on y-axis, 
+# spawns at some random position on x and z-axis
+
+func _on_timer_timeout() -> void:
+	var child = dice.instantiate()
+	child.position = Vector3(randf_range(-rand_pos.x, rand_pos.x), 0, randf_range(-rand_pos.z, rand_pos.z))
+	child.rotation = Vector3(randf_range(-PI, PI), randf_range(-PI, PI), randf_range(-PI, PI))
+	add_child(child)
